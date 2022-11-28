74d10eac6dffd8bf14a8f46c110cc9ada7c9d346
bka-2cycle <114967375+bka-2cycle@users.noreply.github.com>
2022-11-28 15:58:13 -0800

    added notes to script and satrted readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,83 +1,7 @@
-// create variables to hold the character options for all 4
-
-// create an empty array that will hold the users selected characters
-
-// var possibleChar = []
-// var passwordArray = []
-
-// on game start prompt for users # of characters and hold in a variable
-// var numChars = prompt("How many characters would you like in your password?")
-
-// check the users input and confirm they put in a number between 8 and 128 and that it is a number
-
-// write function that validated users input
-
-// function validateUserInput(chars) {
-  // parsInt(chars) make sure that it is an integer, if not return null
-  // if (chars < 8 || chars > 128) {
-  // return null }
-  // }
-
-  // after validation create variables for character selections:
-
-  // var lcChar = confirm("Would you like Lower Case characters?")
-  // var ucChar = confirm("Would you like Upper Case characters?")
-  // var nums = confirm("Would you like Numbers?")
-  // var specChar = confirm("Would you like Special characters?")
-
-  // validate that at least one option was selected
-
-//This could be a function!!!!!! (outside functions that are called)
-// if (!lcChar && !ucChar && !nums && !specChar) {
-  // alert("You need to select at least one type of character");
-  // return generatePassword()
-  // }
-
-  // after validation is complete start pushing selected characters into possibleChar array
-
-  //This could be a function!!!!! (outside functions that are called)
-  // if (lcChar) {
-    // possibleChar.concat(lcCharArray)
-    // }
-
-    // if (ucChar) {
-    // possibleChar.concat(ucCharArray)
-    // }
-
-    // if (nums) {
-    // possibleChar.concat(numsArray)
-    // }
-
-    // if (specChar) {
-    // possibleChar.concat(specCharArray)
-    // }
-
-    // create a random number
-
-    // loop through possibleChar and select a random character from the possibleChar array based on how many characters they entered into the prompt
-
-    // push() the randomly generated character to a empty array called passwordArray
-
-    // return passwordArray.join()
-
- 
-
-
-
-
-
-
-
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-
-
-
 function generatePassword() {
   var lcCharArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var ucCharArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -85,111 +9,61 @@ function generatePassword() {
   var specCharArray = ["!", "#", "$", "%", "&", "(", ")", "*", "+"];
   var possibleChar = [];
   var passwordArray = "";
-  //mine
-  //var finishedPassword = [];
-  //var numChars = [];
 
-  // the value of numChars is stored where, after it is defined by the user? in numChars variable, everytime its referenced thereafter?
-  // or do I create an undeclared var to store that value?  ( var numChars; )
-  
+  // user establishes password length (numChars) 
   var numChars = prompt("How many characters would you like in your password?");
-numChars = parseInt(numChars);
+  numChars = parseInt(numChars);
 
-// validate above input ( Parameters give a name to the data to be passed into the function, in this case "chars" FUNCTION NO WORKIE)
-  // function validateUserInput(chars) {
-                  //parsInt(chars) make sure that it is an integer, if not return null
-  // if (chars < 8 || chars > 128) {
-  // return null; }
-  //}
+  // validate above input
   if (numChars < 8 || numChars > 128) {
     alert("Your password needs to be between 8 and 128 characters!");
     return generatePassword();
-    }
+  }
 
-  console.log(numChars);
-
+  // user chooses from 4 arrays to be included in selection pool (possibleChar)
   var lcChar = confirm("Would you like Lower Case characters?")
   var ucChar = confirm("Would you like Upper Case characters?")
   var nums = confirm("Would you like Numbers?")
   var specChar = confirm("Would you like Special characters?")
-//validate above input
+  // validate above input
   if (!lcChar && !ucChar && !nums && !specChar) {
-  alert("You need to select at least one type of character");
-  return generatePassword()
+    alert("You need to select at least one type of character");
+    return generatePassword()
   }
 
-//after validation complete
-  //start pushing selected characters into possibleChar array
-  //This could be a function!!!!! (an outside function that is called in)
-  
-          //function createUserPool() {
-    if (lcChar) {
-      possibleChar = possibleChar.concat(lcCharArray);
-      //alert("lc was concated!!");
-      //console.log(possibleChar);
-    }
+  // after validation complete add selected characters into possibleChar array
+  if (lcChar) {
+    possibleChar = possibleChar.concat(lcCharArray);
+  }
 
-    if (ucChar) {
-      possibleChar = possibleChar.concat(ucCharArray);
-      //alert("uc was concated!!");
-    }
+  if (ucChar) {
+    possibleChar = possibleChar.concat(ucCharArray);
+  }
 
-    if (nums) {
-      possibleChar = possibleChar.concat(numsArray);
-      //alert("nums was concated!!");
-    }
+  if (nums) {
+    possibleChar = possibleChar.concat(numsArray);
+  }
 
-    if (specChar) {
-      possibleChar = possibleChar.concat(specCharArray);
-      //alert("special was concated!!");
-    }
-          //}
+  if (specChar) {
+    possibleChar = possibleChar.concat(specCharArray);
+  }
 
-            //createUserPool();
-            //var createUserPool = possibleChar;
-
-
-  
-
-    console.log(possibleChar);
-    
-            // testLoop for VERIFY for (var i = 0; i < possibleChar.length; i++) {
-            //console.log("Great to see anything, " + possibleChar[i] + " anything at all");
-            //}
-
-  
-
-  //Instructor suggest: create a random # here 
-    //(why, the character count/length was chosen by user: defined as: var numChars or user input perameter "chars"
-    //maybe difine the results of numChars?
-
-console.log(typeof numChars)
-    
-//loop through possibleChars and select a random character from the possibleChar array based on how many characters they entered into the prompt or---
-    //translated=loop through possibleChar array and select a random character x-times ( x = the # of characters they entered into the prompt for numChars )
-    for (var i = 0; i < numChars; i++) {
+  // loop through possibleChar array and select random characters x(numChars)-times
+  for (var i = 0; i < numChars; i++) {
     index = Math.floor(Math.random() * possibleChar.length);
     passwordArray += possibleChar[index];
-      //var finishedPassword = [i];
-    }
-console.log(passwordArray);
+  }
 
-// push() the randomly generated character(s) to a empty array called passwordArray
-//passwordArray.push([i]);
-
-    return passwordArray;
-    //return passwordArray.join(finishedPassword);
-
-
-  //return finishedPassword;
+  // return finished password
+  return passwordArray;
 }
 
+// display password
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# password-generator
+# Password Generator
 An automated password generator based on criteria you select.
+
+## Description
+
+This web page allows you to create a unique password. The user selects various criteria to be used for the generated password.
+The user selects password length (# of characters), and which character sets to use: Numbers, Special Characters, Upper Case Letters and Lower Case Letters.
+From the selected criteria the computer will choose random characters to create a password.
+
+This is a tool to create a unique password that is secure/safe.
+Its creates complex passwords quickly and efficiently.
+
+During this project I learned how to implement various loops, functions and arrays within Javascript to implement the password generator.
+
+## Installation
+
+N/A
+
+## Usage
+
+Click on the "Generate Password" button.
+
+![alt text](assets/pass-gen-01.png)
+
+Answer prompts for selecting the criteria for your password.
+
+![alt text](assets/pass-gen-02.png)
+
+Once the password is generated you can select/copy for use.
+
+![alt text](assets/pass-gen-03.png)
+
+To create another password, click "Generate Password" and repeat.
+
+
+## Credits
+
+The following web sites were referenced throughout completion of this project: 
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+https://www.w3schools.com/js/default.asp
+
+
+## License
+
+Please refer to the license in the Repo.
