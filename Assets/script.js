ec36be09d19d5e7e46e9b78a2d7e2636495a9b4f
bka-2cycle <114967375+bka-2cycle@users.noreply.github.com>
2022-11-28 16:24:41 -0800

    edited script

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,6 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// write password to the #password input
 function generatePassword() {
   var lcCharArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var ucCharArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
